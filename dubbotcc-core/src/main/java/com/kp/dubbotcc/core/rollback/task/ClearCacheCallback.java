afc3d5832067972a0bb61dcaf254656115283b91
prince399 <chenbin@kuparts.com>
2016-08-06 22:16:42 +0800

    分支

---
+++ dubbotcc-core/src/main/java/com/kp/dubbotcc/core/rollback/task/ClearCacheCallback.java
@@ -0,0 +1,9 @@
+package com.kp.dubbotcc.core.rollback.task;
+
+/**
+ * @author chenbin@kuparts.com
+ * @author chenbin
+ * @version 1.0
+ **/
+public class ClearCacheCallback implements CompensationCallback {
+}
